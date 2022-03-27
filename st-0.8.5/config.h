382fdedc3113b092d931115d9e3be8c97f1ca7c5
zwei <1330544517@qq.com>
2022-03-27 20:57:24 +0800

    change font size 15

--- st-0.8.5/config.h
+++ st-0.8.5/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrainsMono Nerd Font Mono:style=Bold";
+static char *font = "JetBrainsMono Nerd Font Mono:style=Bold:pixelsize=15";
 static int borderpx = 2;
 
 /*
